c07c4c33d787e081b8ed81b1281c06e017df4196
Alexis <ianalexis709@hotmail.com>
2015-06-05 18:43:59 -0500

    m

--- Helloworld/ConsoleApplication1/Program.cs
+++ Helloworld/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World s");
+            Console.WriteLine("Hello World 2");
           
         }
     }

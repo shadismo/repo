b8940936eea616ba251be120f6eec9d4c88f42e5
Alexis <ianalexis709@hotmail.com>
2015-06-05 22:25:43 -0500

    no r

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World X");
+            Console.WriteLine("Hello World W");
           
         }
     }
